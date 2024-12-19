3711b7f39a7d5845a6a8b3c5dc86da29643a225d
Sanjana <sanjanasogimatt@gmail.com>
2024-12-19 16:39:19 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import Download from './sections/Download';
+import Faq from './sections/Faq';
+import { Features } from './sections/Features';
+import Footer from './sections/Footer';
+import Header from './sections/Header';
+import Hero from './sections/Hero';
+import Pricing from './sections/Pricing';
+import Testimonials from './sections/Testimonials';
+const App =()=>{
+    return(
+        <div className="overflow-hidden">
+            <Header />
+            <Hero />
+            <Features />
+            <Pricing />
+            <Faq />
+            <Testimonials />
+            <Download />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
